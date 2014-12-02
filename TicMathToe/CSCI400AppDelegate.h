1901158a6e0a2e8cf607c99311875ac6eb70ee9b
Begley <mjb09755@b101-013.academia.sjfc.edu>
2014-12-02 13:38:26 -0500

    Initial Commit

---
+++ TicMathToe/CSCI400AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CSCI400AppDelegate.h
+//  TicMathToe
+//
+//  Created by Begley, Matthew J on 2014-12-02.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSCI400AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TicMathToe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TicMathToe
+//
+//  Created by Begley, Matthew J on 2014-12-02.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CSCI400AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CSCI400AppDelegate class]));
+    }
+}
